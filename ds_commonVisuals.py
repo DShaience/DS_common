1b6ed91f3444fe7761949d9b8372063e1a666d3d
Shay <shay.teaching@gmail.com>
2020-04-12 16:20:23 +0300

    Refactored correlation matrix code

--- ds_commonVisuals.py
+++ ds_commonVisuals.py
@@ -1,12 +1,37 @@
 import itertools
-from typing import List, Tuple
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+from typing import List, Union
+from scipy.stats import ks_2samp, gaussian_kde
+sns.set(color_codes=True)
 
 
-def correlationMatrix(df: pd.DataFrame, fontsz: int = 16, toShow: bool = True):
+def plotMatrix(mat: Union[pd.DataFrame, np.ndarray], fontsz: int, cbar_ticks: List[float] = None):
+    """
+    :param mat: matrix to plot. If using dataframe, the columns are automatically used as labels. Othereise, matrix is anonymous
+    :param fontsz: font size
+    :param cbar_ticks: the spacing between cbar ticks. If None, this is set automatically.
+    :return:
+    """
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    plt.figure(figsize=[8, 8])
+    if cbar_ticks is not None:
+        ax = sns.heatmap(mat, cmap=cmap, vmin=min(cbar_ticks), vmax=max(cbar_ticks), square=True, linewidths=.5, cbar_kws={"shrink": .5})
+        cbar = ax.collections[0].colorbar
+        cbar.set_ticks(cbar_ticks)
+        cbar.set_ticklabels(cbar_ticks)
+    else:
+        ax = sns.heatmap(mat, cmap=cmap, vmin=np.min(np.array(mat).ravel()), vmax=np.max(np.array(mat).ravel()), square=True, linewidths=.5, cbar_kws={"shrink": .5})
+        cbar = ax.collections[0].colorbar
+
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=90, fontsize=fontsz)
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0, fontsize=fontsz)
+    plt.show()
+
+
+def correlationMatrix(df: pd.DataFrame, fontsz: int = 16, corrThr: float = None):
     """
     :param df: input dataframe. Correlation matrix calculated for all columns
     :param fontsz: font size
@@ -17,21 +42,28 @@ def correlationMatrix(df: pd.DataFrame, fontsz: int = 16, toShow: bool = True):
     cols_numeric = list(df)
     data_numeric = df[cols_numeric].copy(deep=True)
     corr_mat = data_numeric.corr(method='pearson')
+    if corrThr is not None:
+        assert corr_mat > 0.0, "corrThr must be a float between [0, 1]"
+        corr_mat[corr_mat >= corrThr] = 1.0
+        corr_mat[corr_mat <= -corrThr] = -1.0
+
     cbar_ticks = [round(num, 1) for num in np.linspace(-1, 1, 11, dtype=np.float)]  # rounding corrects for floating point imprecision
-
-    cmap = sns.diverging_palette(220, 10, as_cmap=True)
-    plt.figure(figsize=[8, 8])
-    plt.xticks(fontsize=fontsz)
-    plt.yticks(fontsize=fontsz)
-    ax = sns.heatmap(corr_mat, cmap=cmap, vmin=-1, vmax=1, square=True, linewidths=.5, cbar_kws={"shrink": .5})
-    cbar = ax.collections[0].colorbar
-    cbar.set_ticks(cbar_ticks)
-    cbar.set_ticklabels(cbar_ticks)
-    if toShow:
-        plt.show()
+    plotMatrix(corr_mat, fontsz=fontsz, cbar_ticks=cbar_ticks)
+    # def plotMatrix(mat: np.ndarray, fontsz: int, cbar_ticks: np.ndarray = None):
 
 
-def plot_confusion_matrix(cm: np.array, label_names: List[str], title: str='Confusion matrix', cmaptype=0):
+    # cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    # plt.figure(figsize=[8, 8])
+    # plt.xticks(fontsize=fontsz)
+    # plt.yticks(fontsize=fontsz)
+    # ax = sns.heatmap(corr_mat, cmap=cmap, vmin=-1, vmax=1, square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    # cbar = ax.collections[0].colorbar
+    # cbar.set_ticks(cbar_ticks)
+    # cbar.set_ticklabels(cbar_ticks)
+    # plt.show()
+
+
+def plotConfusionMatrix(cm: np.array, label_names: List[str], title: str='Confusion matrix', cmaptype=0):
     '''
     :param cm:
     :param label_names:
